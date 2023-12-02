484a8768681e1e0b2b84f7ae49115acd971962fd
abinash <abinashphulkonwar1@gmail.com>
2023-12-02 22:58:33 +0530

    add log struct and file

--- logger/logger.go
+++ logger/logger.go
@@ -1,15 +1,34 @@
 package logger
 
-import "github.com/abinashphulkonwar/route-master/services"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/abinashphulkonwar/route-master/services"
+)
 
 type Logger struct {
 	queue *services.Queue
+	file  *os.File
+}
+
+type Log struct {
+	Method  string
+	Path    string
+	Address string
+	Scheme  string
+	Name    string
 }
 
 func NewLogger() *Logger {
 	newQueue := services.NewQueue()
+	file, err := os.OpenFile("log.json", os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
 	logger := &Logger{
 		queue: newQueue,
+		file:  file,
 	}
 
 	go logger.start()
@@ -17,17 +36,29 @@ func NewLogger() *Logger {
 	return logger
 }
 
-func (l *Logger) Log(msg string) {
-	go l.queue.Enqueue([]byte(msg))
+func (l *Logger) Log(event *Log) {
+	data, err := json.Marshal(event)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	go l.queue.Enqueue([]byte(data))
 }
 
 func (l *Logger) start() {
+	defer l.file.Close()
+	endLine := []byte("\n")
+
 	for {
 		isFound, list := l.queue.DequeueMany(5)
 		if isFound {
 
 			for _, log := range *list {
+				log = append(log, endLine...)
 				println(string(log))
+
+				l.file.Write(log)
 			}
 
 		}
