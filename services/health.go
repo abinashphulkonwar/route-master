5b6fc07168959ee80d60949dd32ca66c671bda1e
abinash <abinashphulkonwar1@gmail.com>
2023-12-06 22:48:50 +0530

    add health check

--- services/health.go
+++ services/health.go
@@ -3,7 +3,15 @@ package services
 import (
 	"net/http"
 	"sync"
+	"time"
 )
+
+type NodeHealth struct {
+	Name      string
+	Success   int
+	Error     int
+	updatedAt time.Time
+}
 
 type Health struct {
 	config *Config
@@ -11,34 +19,73 @@ type Health struct {
 }
 
 func NewHealth(config *Config) *Health {
-
 	health := Health{
 		config: config,
 		hMap:   sync.Map{},
 	}
-	go health.check()
+	go health.checkHealth()
 	return &health
 }
 
-func (h *Health) check() error {
-	for _, node := range h.config.Node {
-		for _, url := range node.Target {
-			request, err := http.NewRequest("GET", url, nil)
-			if err != nil {
-				println(err.Error())
-				continue
-			}
-			res, err := http.DefaultClient.Do(request)
-			if err != nil {
-				println(err.Error())
-				continue
-			}
-			if res.StatusCode != 200 {
-				println("status code is not 200")
-				continue
-			}
+func (h *Health) check(node string, url string, healthCheckPath string) {
 
+	request, err := http.NewRequest("GET", url+healthCheckPath, nil)
+	if err != nil {
+		h.hSet(node+":"+url, false)
+		println(err.Error())
+		return
+	}
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		h.hSet(node+":"+url, false)
+		println(err.Error())
+		return
+	}
+	if res.StatusCode == 200 {
+		h.hSet(node+":"+url, true)
+	} else {
+		h.hSet(node+":"+url, false)
+		println("status code is not 200")
+
+	}
+
+}
+
+func (h *Health) checkHealth() {
+	for {
+		for _, node := range h.config.Node {
+			for _, url := range node.Target {
+				h.check(node.Name, node.Scheme+url, node.Health)
+			}
 		}
 	}
-	return nil
+}
+
+func (h *Health) hSet(key string, status bool) {
+
+	val, isFound := h.hMap.Load(key)
+	if isFound {
+		node := val.(*NodeHealth)
+		if status {
+			node.Success++
+		} else {
+			node.Error++
+		}
+		return
+	}
+
+	node := NodeHealth{
+		Name:      key,
+		Success:   0,
+		Error:     0,
+		updatedAt: time.Now(),
+	}
+	if status {
+		node.Success++
+	} else {
+		node.Error++
+	}
+
+	h.hMap.Store(key, &node)
+
 }
